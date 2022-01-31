8acb34758e3c410b60dcc22889ec04367b200ae7
Ruddy Setiadi Gunawan <dary1039gbz@outlook.com>
2022-01-31 16:21:40 -0500

    cardano update

--- src/pages/Home/components/Accessibility/index.tsx
+++ src/pages/Home/components/Accessibility/index.tsx
@@ -1,4 +1,5 @@
 import {ReactComponent as Polygon} from '../../../../assets/Icons/polygon.svg';
+import {ReactComponent as Cardano} from '../../../../assets/Icons/cardano.svg';
 import {ReactComponent as Wave} from '../../../../assets/images/wave.svg';
 import Spacing from '../../../../components/Spacing';
 import Text from '../../../../components/Typography';
@@ -10,7 +11,7 @@ export const Accessibility = () => {
 			<p className="section-text__title--small small">BUILT FOR ACCESSIBILITY</p>
 
 			<Text className="powered-by-text">
-				<span>Powered by</span> <a href="https://polygon.technology/" target="_blank" rel="noreferrer"><Polygon /></a>
+				 <a href="https://polygon.technology/" target="_blank" rel="noreferrer"><Polygon /></a><Cardano />
 			</Text>
 
 			<p className="section-text__description description">
