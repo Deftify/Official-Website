c515f24c5a4707a7417d0f9d9fd8f4751a243674
Ruddy Setiadi Gunawan <dary1039gbz@outlook.com>
2022-02-21 17:30:53 -0500

    update

--- src/pages/Home/components/PrivateAccessPad/index.tsx
+++ src/pages/Home/components/PrivateAccessPad/index.tsx
@@ -60,7 +60,7 @@ const SectionOne = () => <Spacing className="section-one">
 			Deftify token (DFTY) has multiple use cases across Deftify ecosystem
 		</p>
 		<div className="stakedfty_image stakedfty_image__stake">
-		<img className="stake-image" src="./images/stakedfty-transparent.svg" alt={''} /></div>
+		<img className="stake-image" src="./images/stakedfty-transparent2.svg" alt={''} /></div>
 		&nbsp;
 		<p className="section-text__header2 header2">
 			Private Access Pad (PAP)
