92eddec492326506b7923eba3529edbb8ab736ae
Ruddy Setiadi Gunawan <dary1039gbz@outlook.com>
2022-05-03 22:54:08 -0500

    update

--- src/pages/Home/components/Accessibility/index.tsx
+++ src/pages/Home/components/Accessibility/index.tsx
@@ -1,8 +1,25 @@
 import {ReactComponent as AllChains} from '../../../../assets/Icons/allchains.svg';
+import {ReactComponent as Polygon} from '../../../../assets/Icons/polygon.svg';
 import {ReactComponent as Wave} from '../../../../assets/images/wave.svg';
+import React, { useEffect } from 'react';
 import Spacing from '../../../../components/Spacing';
 import Text from '../../../../components/Typography';
 import './index.scss';
+
+useEffect(() => {
+	const checkWidth = () => {
+		if (window.innerWidth <= 1023) {
+			setDeviceWidth('mobile');
+		}
+		if (window.innerWidth > 1023) {
+			setDeviceWidth('desktop');
+		}
+	};
+	window.addEventListener('resize', checkWidth, { passive: true });
+	checkWidth();
+	return () => window.removeEventListener('resize', checkWidth);
+}, []);
+
 
 export const Accessibility = () => {
 	return (
@@ -12,7 +29,14 @@ export const Accessibility = () => {
 			<Text className="powered-by-text">
 			<div className="row">
 			<div className="column">
-			<AllChains />
+			{deviceWidth === 'mobile' ? (
+                <Polygon />
+                ) : deviceWidth === 'desktop' ? (
+				<AllChains />
+                ) : (
+                    ''
+                )}
+
   			</div>
   			</div>
 			</Text>
