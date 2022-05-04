757ff7cc43822b8d12f90848992d760fbcb7f07e
Ruddy Setiadi Gunawan <dary1039gbz@outlook.com>
2022-05-03 22:03:49 -0500

    update evm

--- src/pages/Home/components/Accessibility/index.tsx
+++ src/pages/Home/components/Accessibility/index.tsx
@@ -1,5 +1,6 @@
 import {ReactComponent as Polygon} from '../../../../assets/Icons/polygon.svg';
 import {ReactComponent as Cardano} from '../../../../assets/Icons/cardano.svg';
+import {ReactComponent as EVM} from '../../../../assets/Icons/evm.svg';
 import {ReactComponent as Wave} from '../../../../assets/images/wave.svg';
 import Spacing from '../../../../components/Spacing';
 import Text from '../../../../components/Typography';
@@ -16,6 +17,10 @@ export const Accessibility = () => {
 			<a href="https://polygon.technology/" target="_blank" rel="noreferrer"><Polygon /></a>
   			</div>
 			  <div className="column">&nbsp;</div>
+  			<div className="column">
+  			<EVM />
+  			</div>
+			<div className="column">&nbsp;</div>
   			<div className="column">
   			<Cardano />
   			</div>
